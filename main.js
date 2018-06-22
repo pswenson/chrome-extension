fdbddabe7a8dcfc69d3505428f40ffd6480bb8dd
phil swenson <philip_swenson@comcast.com>
2018-06-22 13:54:59 -0600

    wip

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
-function getJIRAFeed(callback, errorCallback){
-    var user = document.getElementById("user").value;
-    if(user == undefined) return;
-    
-    var url = "https://jira.secondlife.com/activity?maxResults=50&streams=user+IS+"+user+"&providers=issues";
-    make_request(url, "").then(function(response) {
-      // empty response type allows the request.responseXML property to be returned in the makeRequest call
-      callback(url, response);
-    }, errorCallback);
+function getJIRAFeed(callback, errorCallback) {
+  var user = document.getElementById("user").value;
+  if (user == undefined) return;
+
+  var url = "https://jira.secondlife.com/activity?maxResults=50&streams=user+IS+" + user + "&providers=issues";
+  make_request(url, "").then(function (response) {
+    // empty response type allows the request.responseXML property to be returned in the makeRequest call
+    callback(url, response);
+  }, errorCallback);
 }
+
 /**
  * @param {string} searchTerm - Search term for JIRA Query.
- * @param {function(string)} callback - Called when the query results have been  
+ * @param {function(string)} callback - Called when the query results have been
  *   formatted for rendering.
  * @param {function(string)} errorCallback - Called when the query or call fails.
  */
-async function getQueryResults(s, callback, errorCallback) {                                                 
-    try {
-      var response = await make_request(s, "json");
-      callback(createHTMLElementResult(response));
-    } catch (error) {
-      errorCallback(error);
-    }
+async function getQueryResults(s, callback, errorCallback) {
+  try {
+    var response = await make_request(s, "json");
+    //todo this doesn't belong here, got back to getQueryResults caller and handle there
+    callback(displayQueryResults(response));
+  } catch (error) {
+    errorCallback(error);
+  }
 }
 
 function make_request(url, responseType) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     var req = new XMLHttpRequest();
     req.open('GET', url);
     req.responseType = responseType;
 
-    req.onload = function() {
+    req.onload = function () {
       var response = responseType ? req.response : req.responseXML;
-      if(response && response.errorMessages && response.errorMessages.length > 0){
+      if (response && response.errorMessages && response.errorMessages.length > 0) {
         reject(response.errorMessages[0]);
         return;
       }
@@ -39,12 +41,12 @@ function make_request(url, responseType) {
     };
 
     // Handle network errors
-    req.onerror = function() {
+    req.onerror = function () {
       reject(Error("Network Error"));
     }
-    req.onreadystatechange = function() { 
-      if(req.readyState == 4 && req.status == 401) { 
-          reject("You must be logged in to JIRA to see this project.");
+    req.onreadystatechange = function () {
+      if (req.readyState == 4 && req.status == 401) {
+        reject("You must be logged in to JIRA to see this project.");
       }
     }
 
@@ -54,120 +56,132 @@ function make_request(url, responseType) {
 }
 
 
-
-function loadOptions(){
+function loadSavedOptions() {
   chrome.storage.sync.get({
     project: 'Sunshine',
     user: 'nyx.linden'
-  }, function(items) {
+  }, function (items) {
     document.getElementById('project').value = items.project;
     document.getElementById('user').value = items.user;
   });
 }
-function buildJQL(callback) {
+
+//todo should these be constants?
+// build the jira query
+//todo ideally pass in values here instead of reading from document
+function buildJQL() {
   var callbackBase = "https://jira.secondlife.com/rest/api/2/search?jql=";
   var project = document.getElementById("project").value;
   var status = document.getElementById("statusSelect").value;
   var inStatusFor = document.getElementById("daysPast").value
-  var fullCallbackUrl = callbackBase;
-  fullCallbackUrl += `project=${project}+and+status=${status}+and+status+changed+to+${status}+before+-${inStatusFor}d&fields=id,status,key,assignee,summary&maxresults=100`;
-  callback(fullCallbackUrl);
+  let jqlUrl = `${callbackBase}project=${project}+and+status=${status}+and+status+changed+to+${status}+before+-${inStatusFor}d&fields=id,status,key,assignee,summary&maxresults=100`;
+  return jqlUrl;
 }
-function createHTMLElementResult(response){
 
+function displayQueryResults(response) {
+//todo parse the response + create HTML
 // 
 // Create HTML output to display the search results.
 // results.json in the "json_results" folder contains a sample of the API response
 // hint: you may run the application as well if you fix the bug. 
 // 
+  console.log("response=", response);
+  console.log("issues=", response.issues);
 
-  return '<p>There may be results, but you must read the response and display them.</p>';
-  
+
+
+  return '<p>There may be results, but you must read the response and display them!</p>';
+
 }
 
 // utility 
-function domify(str){
-  var dom = (new DOMParser()).parseFromString('<!doctype html><body>' + str,'text/html');
+function domify(str) {
+  var dom = (new DOMParser()).parseFromString('<!doctype html><body>' + str, 'text/html');
   return dom.body.textContent;
 }
 
-function checkProjectExists(){
-    try {
-      return await make_request("https://jira.secondlife.com/rest/api/2/project/SUN", "json");
-    } catch (errorMessage) {
-      document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
-      document.getElementById('status').hidden = false;
-    }
+
+async function checkProjectExists() {
+  console.log("HI!");
+  try {
+    //todo the SUN project is hard-coded
+    return await make_request("https://jira.secondlife.com/rest/api/2/project/SUN", "json");
+    //todo force error and see what happens
+  } catch (errorMessage) {
+    document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
+    document.getElementById('status').hidden = false;
+  }
 }
 
 // Setup
-document.addEventListener('DOMContentLoaded', function() {
+//todo this function is way too big, break it up!
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("DOM!");
   // if logged in, setup listeners
-    checkProjectExists().then(function() {
-      //load saved options
-      loadOptions();
+  //TODO handle if project doesn't exist, show error message?
 
-      // query click handler
-      document.getElementById("query").onclick = function(){
-        // build query
-        buildJQL(function(url) {
-          document.getElementById('status').innerHTML = 'Performing JIRA search for ' + url;
-          document.getElementById('status').hidden = false;  
-          // perform the search
-          getQueryResults(url, function(return_val) {
-            // render the results
-            document.getElementById('status').innerHTML = 'Query term: ' + url + '\n';
-            document.getElementById('status').hidden = false;
-            
-            var jsonResultDiv = document.getElementById('query-result');
-            jsonResultDiv.innerHTML = return_val;
-            jsonResultDiv.hidden = false;
+  checkProjectExists().then(function () {
+    loadSavedOptions();
+    // query click handler
+    document.getElementById("query").onclick = function () {
+      // build query
+      let jiraQueryUrl = buildJQL();
+      document.getElementById('status').innerHTML = 'Performing JIRA search for ' + jiraQueryUrl;
+      document.getElementById('status').hidden = false;
+      // perform the search
+      getQueryResults(jiraQueryUrl, function (return_val) {
+        // render the results
+        document.getElementById('status').innerHTML = 'Query term: ' + jiraQueryUrl + '\n';
+        document.getElementById('status').hidden = false;
 
-          }, function(errorMessage) {
-              document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
-              document.getElementById('status').hidden = false;
-          });
-        });
-      }
+        var jsonResultDiv = document.getElementById('query-result');
+        jsonResultDiv.innerHTML = return_val;
+        jsonResultDiv.hidden = false;
 
-      // activity feed click handler
-      document.getElementById("feed").onclick = function(){   
-        // get the xml feed
-        getJIRAFeed(function(url, xmlDoc) {
-          document.getElementById('status').innerHTML = 'Activity query: ' + url + '\n';
-          document.getElementById('status').hidden = false;
-
-          // render result
-          var feed = xmlDoc.getElementsByTagName('feed');
-          var entries = feed[0].getElementsByTagName("entry");
-          var list = document.createElement('ul');
-
-          for (var index = 0; index < entries.length; index++) {
-            var html = entries[index].getElementsByTagName("title")[0].innerHTML;
-            var updated = entries[index].getElementsByTagName("updated")[0].innerHTML;
-            var item = document.createElement('li');
-            item.innerHTML = new Date(updated).toLocaleString() + " - " + domify(html);
-            list.appendChild(item);
-          }
-
-          var feedResultDiv = document.getElementById('query-result');
-          if(list.childNodes.length > 0){
-            feedResultDiv.innerHTML = list.outerHTML;
-          } else {
-            document.getElementById('status').innerHTML = 'There are no activity results.';
-            document.getElementById('status').hidden = false;
-          }
-          
-          feedResultDiv.hidden = false;
-
-        }, function(errorMessage) {
-          document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
-          document.getElementById('status').hidden = false;
-        });    
-      };        
-
-    }).catch(function(errorMessage) {
+      }, function (errorMessage) {
         document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
         document.getElementById('status').hidden = false;
-    });   
+      });
+    }
+
+    // activity feed click handler
+    document.getElementById("feed").onclick = function () {
+      // get the xml feed
+      getJIRAFeed(function (url, xmlDoc) {
+        document.getElementById('status').innerHTML = 'Activity query: ' + url + '\n';
+        document.getElementById('status').hidden = false;
+
+        // render result
+        var feed = xmlDoc.getElementsByTagName('feed');
+        var entries = feed[0].getElementsByTagName("entry");
+        var list = document.createElement('ul');
+
+        for (var index = 0; index < entries.length; index++) {
+          var html = entries[index].getElementsByTagName("title")[0].innerHTML;
+          var updated = entries[index].getElementsByTagName("updated")[0].innerHTML;
+          var item = document.createElement('li');
+          item.innerHTML = new Date(updated).toLocaleString() + " - " + domify(html);
+          list.appendChild(item);
+        }
+
+        var feedResultDiv = document.getElementById('query-result');
+        if (list.childNodes.length > 0) {
+          feedResultDiv.innerHTML = list.outerHTML;
+        } else {
+          document.getElementById('status').innerHTML = 'There are no activity results.';
+          document.getElementById('status').hidden = false;
+        }
+
+        feedResultDiv.hidden = false;
+
+      }, function (errorMessage) {
+        document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
+        document.getElementById('status').hidden = false;
+      });
+    };
+
+  }).catch(function (errorMessage) {
+    document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
+    document.getElementById('status').hidden = false;
+  });
 });
--- options.js
+++ options.js
@@ -1,3 +1,6 @@
+//todo move functions out of callback and reference them
+
+
 // Saves options to chrome.storage
 function save_options() {
   var prj = document.getElementById('project').value;
@@ -5,12 +8,12 @@ function save_options() {
   chrome.storage.sync.set({
     project: prj,
     user: usr
-  }, function() {
+  }, function () {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     status.hidden = false;
-    setTimeout(function() {
+    setTimeout(function () {
       status.textContent = '';
       status.hidden = true;
     }, 1000);
@@ -19,15 +22,18 @@ function save_options() {
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
+//todo project and user are hard-coded
 function restore_options() {
   chrome.storage.sync.get({
     project: 'Sunshine',
     user: 'nyx.linden'
-  }, function(items) {
+  }, function (items) {
     document.getElementById('project').value = items.project;
     document.getElementById('user').value = items.user;
   });
 
   document.getElementById('save').addEventListener('click', save_options);
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
+console.log("yikes");
